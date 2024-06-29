f0b6fb3e344deb54cdf9e32450b5c2664c064a56
Krissny <110375406+Krissny@users.noreply.github.com>
2024-06-29 20:51:58 +0530

    Add routing to Buy button

--- src/Utility/add.js
+++ src/Utility/add.js
@@ -1,5 +1,11 @@
-export default function add(cart, setCart, title, image, price) {
-  if (cart.find((obj) => obj.title == title)) {
+export default function add(cart, setCart, title, image, price, buy = false) {
+  if (buy) {
+    if (!cart.find((obj) => obj.title == title)) {
+      setCart([...cart, { title, image, price, quantity: 1 }]);
+    } else {
+      return;
+    }
+  } else if (cart.find((obj) => obj.title == title)) {
     let list = [...cart];
     list = list.filter((obj) => obj.title != title);
     setCart(list);
--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import styles from "./Card.module.css";
 import add from "../../Utility/add";
+import { Link } from "react-router-dom";
 
 export default function Card({ title, image, price, cart, setCart }) {
   // const [added, setAdded] = useState(cart.find((obj) => obj.title == title));
@@ -35,7 +36,13 @@ export default function Card({ title, image, price, cart, setCart }) {
         }}
       >{`$ ${price}`}</p>
       <div className={styles.btnCont}>
-        <button className={styles.buy}>Buy</button>
+        <Link
+          onClick={() => add(cart, setCart, title, image, price, true)}
+          className={styles.buy}
+          to="/checkout"
+        >
+          Buy
+        </Link>
         <button
           className={styles.add}
           onClick={() => add(cart, setCart, title, image, price)}
